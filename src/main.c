488bc3f8684ffb1e13782b797463a4a01dcb5308
Tanay <tanaymatta27@gmail.com>
2025-03-08 18:29:45 +0400

    comments lol

--- src/main.c
+++ src/main.c
@@ -5,11 +5,12 @@
 double WEIGHTS[] = {-2.00,5.63};
 
 
-int main(int arg, char ** args){
+int main(int arg, char **args){
+    
+    // ONE INPUT NODE, ONE OUTPUT NODE
     printf("one in one out solution: %f\n",one_in_one_out(2.143,-2.00));
-    
-    
-    
+
+    // ONE INPUT NODE, MULTIPLE OUTPUT NODES
     double *result = one_in_multiple_out(5.63,WEIGHTS,2);
     for (int i = 0; i<2; i++){
         printf("one in multiple out solution for output node %d: %f\n",i, result[i]);
